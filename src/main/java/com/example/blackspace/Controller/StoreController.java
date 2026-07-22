824051cdb95f2e9ff0874b66eafb96632e2e208b
Stephen Zulu <zulusteven9@gmail.com>
2026-07-22 20:56:02 +0200

    Update delete function

--- src/main/java/com/example/blackspace/Controller/StoreController.java
+++ src/main/java/com/example/blackspace/Controller/StoreController.java
@@ -9,6 +9,8 @@ import com.example.blackspace.Service.User.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -227,8 +229,17 @@ public class StoreController {
         return "redirect:/admin/stores";
     }
 
-
-
+    @DeleteMapping("/admin/stores/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<String> deleteStore(@PathVariable Long id) {
+        try {
+            storeService.deleteStore(id);
+            return ResponseEntity.ok("Deleted successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error deleting store: " + e.getMessage());
+        }
+    }
 
 
 
